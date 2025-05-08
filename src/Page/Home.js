1c6dddbc7c13bd3d1153b58c31ac9041601b899e
wbkong <hmy1219@naver.com>
2025-05-08 17:03:58 +0900

    1. 경력쪽 틀 완성 2. 내용은 바꿔야함 3. 스트링도 포함

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -41,35 +41,56 @@ function Home() {
                 </div>
 
 
-                <div>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
-                    <p>경력 n년</p>
+                <div className="resume-container">
+                    <div className="resume-left">
+                        <h3>경력 (총 7년)</h3>
+                        <p><strong>Neurophet</strong> 2024.07 ~ 현재</p>
+                        <ul>
+                            <li>Data Validation Tools 개발</li>
+                            <li>SCALE CTP 검증</li>
+                        </ul>
 
+                        <p><strong>NHN KCP</strong> 2024.02 ~ 2024.07</p>
+                        <ul>
+                            <li>"Cherry POS" QA 진행</li>
+                            <li>"SPL" (해외 결제) Test 진행</li>
+                        </ul>
+
+                        <p><strong>슈프리마</strong> 2022.02 ~ 2024.02</p>
+                        <ul>
+                            <li>“CLuE” 서비스 출시 및 QA 진행</li>
+                            <li>“Suprema Middle Ware” QA 진행</li>
+                        </ul>
+
+                        <p><strong>와이드넷</strong> 2018.08 ~ 2022.02</p>
+                        <ul>
+                            <li>JLR Tmap for Car QA 진행</li>
+                            <li>RSM Tmap for Car QA 진행</li>
+                        </ul>
+                    </div>
+
+                    <div className="resume-right">
+                        <h3>학력</h3>
+                        <p>연성대학교 정보통신공학 2010.03 ~ 2016.02</p>
+
+                        <h3>자격증</h3>
+                        <ul>
+                            <li>컴퓨터 응용 밀링 기능사 2009.02</li>
+                            <li>문서실무사 1급 2009.03</li>
+                        </ul>
+
+                        <h3>외국어</h3>
+                        <p>비즈니스 회화 OPIc IM2 2021.11</p>
+
+                        <h3>스터디</h3>
+                        <ul>
+                            <li>정보기술개발 응용 SW양성과정 - 에이콘 아카데미 (2016.12 ~ 2017.06)</li>
+                            <li>데이터베이스 심화과정 - 에이콘 아카데미</li>
+                            <li>안드로이드를 활용한 웨어러블 개발 - 에이콘 아카데미</li>
+                        </ul>
+                    </div>
                 </div>
+
 
 
             </div>
